69471ea3fa0a8654325904d62650df44314590ba
Thomas Durieux <durieuxthomas@hotmail.com>
2014-10-31 09:50:35 +0100

    Test the Spoon processor DepencencyAnalyzer

---
+++ src/testProject/java/github/tdurieux/testProject/entity/User.java
@@ -0,0 +1,48 @@
+package github.tdurieux.testProject.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 2603156267507828552L;
+
+	public enum Type {
+		USER, ADMIN
+	}
+
+	private String name;
+	private int id;
+	private Type type = Type.USER;
+
+	public String print(Class object) {
+
+		return object.getSimpleName();
+	}
+	
+	public void thread() {
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				print(this.getClass());
+			}
+		}).run();
+	}
+
+	public void throwException() throws Exception {
+		throw new RuntimeException("");
+	}
+
+	public boolean isInteger(Object o) {
+		if (o instanceof Integer) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+
+		return "[ " + id + "] " + name;
+	}
+
+}
