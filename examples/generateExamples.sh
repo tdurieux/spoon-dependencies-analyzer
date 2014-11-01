ed429a1c09e44e968f161e3b4a16f5ec03f50f7e
Thomas Durieux <durieuxthomas@hotmail.com>
2014-11-01 16:32:46 +0100

    Add Nopol in example generator

--- examples/generateExamples.sh
+++ examples/generateExamples.sh
@@ -3,15 +3,16 @@ java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ../VLille/src -f dot -l c
 java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ../VLille/src -f txt -l package > examples/vlille-DependencyAnalyser.p2p
 java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ../VLille/src -f dot -l package > examples/vlille-DependencyAnalyser.p2p.gv
 
-java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ../spoon-core/src -f txt -l class > examples/spoon-DependencyAnalyser.c2c
-java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ../spoon-core/src -f dot -l class > examples/spoon-DependencyAnalyser.c2c.gv
-java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ../spoon-core/src -f txt -l package > examples/spoon-DependencyAnalyser.p2p
-java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ../spoon-core/src -f dot -l package > examples/spoon-DependencyAnalyser.p2p.gv
-
 java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ./src -f txt -l class > examples/dependencyAnalyser-DependencyAnalyser.c2c
 java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ./src -f dot -l class > examples/dependencyAnalyser-DependencyAnalyser.c2c.gv
 java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ./src -f txt -l package > examples/dependencyAnalyser-DependencyAnalyser.p2p
 java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ./src -f dot -l package > examples/dependencyAnalyser-DependencyAnalyser.p2p.gv
+
+
+java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ../spoon-core/src -f txt -l class > examples/spoon-DependencyAnalyser.c2c
+java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ../spoon-core/src -f dot -l class > examples/spoon-DependencyAnalyser.c2c.gv
+java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ../spoon-core/src -f txt -l package > examples/spoon-DependencyAnalyser.p2p
+java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ../spoon-core/src -f dot -l package > examples/spoon-DependencyAnalyser.p2p.gv
 
 java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ../nopol/nopol/src/ -f dot -l package > examples/nopol-DependencyAnalyzer.p2p.gv
 java -jar target/DependencyAnalyzer-0.0.1-SNAPSHOT.jar ../nopol/nopol/src/ -f txt -l package > examples/nopol-DependencyAnalyzer.p2p
